e1cbf1fe1e1ecc2492607804e3ca0245cbfbc5f7
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2023-10-07 20:42:14 +1100

    Fixed the bug where the timer wasn't alerting us correctly and was counting down to the negatives. Fixed this by adding remainingSeconds in our dependancy list

--- IOSTimer/screens/Timer.js
+++ IOSTimer/screens/Timer.js
@@ -1,14 +1,66 @@
 import { View, Text } from "react-native";
 import { styles } from "./Timer.styles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { formatTimeFunction } from "../Utility/formatTime";
+import { RoundedButton } from "../components/RoundedButton";
 
 export default function Timer() {
-  const [isRunning, setIsRunning] = useState(false)
-  const [remainingSeconds, setRemainingSeconds] = useState(0)
+  const [isRunning, setIsRunning] = useState(false);
+  const [remainingSeconds, setRemainingSeconds] = useState(10);
+
+  let intervalId = null;
+
+  useEffect(() => {
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        if (remainingSeconds > 0) {
+          setRemainingSeconds((prevTime) => prevTime - 1);
+        } else {
+          setIsRunning(false);
+          alert("The timer has finished");
+        }
+      }, 1000); // run that for every 1000ms or second
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isRunning, remainingSeconds]);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.elapsedTime}>{remainingSeconds}</Text>
+      <Text style={styles.elapsedTime}>
+        {formatTimeFunction(remainingSeconds)}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <RoundedButton
+          text={"Cancel"}
+          textColour={"white"}
+          buttonColour={"grey"}
+          onPressFunction={() => {
+            setIsRunning(false);
+            setRemainingSeconds(0);
+          }}
+          disabled={isRunning || remainingSeconds === 0}
+        />
+        {isRunning ? (
+          <RoundedButton
+            text={"Pause"}
+            textColour={"white"}
+            buttonColour={"darkorange"}
+            onPressFunction={() => {
+              setIsRunning(false);
+            }}
+          />
+        ) : (
+          <RoundedButton
+            text={"Start"}
+            textColour={"lime"}
+            buttonColour={"green"}
+            onPressFunction={() => {
+              setIsRunning(true);
+            }}
+          />
+        )}
+      </View>
     </View>
   );
 }
